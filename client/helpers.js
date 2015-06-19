9b675a9c782c65cee4f9ea3915bee7e0ad1d37bd
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2015-06-19 14:19:18 +0300

    add some helpers

--- client/helpers.js
+++ client/helpers.js
@@ -2,8 +2,18 @@ Template.registerHelper('hasPermission', function (permission) {
   return Permissions.has(Meteor.userId(), permission);
 });
 
+Template.registerHelper('hasAllPermissions', function (permissions) {
+  permissions = permissions.split('|');
+
+  return Permissions.hasAllFrom(Meteor.userId(), permissions);
+});
+
 Template.registerHelper('hasAnyPermission', function (permissions) {
   permissions = permissions.split('|');
 
   return Permissions.hasAnyFrom(Meteor.userId(), permissions);
+});
+
+Template.registerHelper('isAdmin', function () {
+  return Permissions.isAdmin(Meteor.userId());
 });
